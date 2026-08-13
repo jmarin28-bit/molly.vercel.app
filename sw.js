d1635d06f9debc32499155f1139775ca9ac24bf6
jmarin28-bit <jmarin28@gmail.com>
2026-08-13 14:54:57 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE = 'molly-v1';
+const CORE = ['/', '/index.html', '/manifest.webmanifest', '/icon-192.png', '/icon-512.png'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(CORE)));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  if (e.request.mode === 'navigate') {
+    e.respondWith(fetch(e.request).then(r => {
+      const copy = r.clone(); caches.open(CACHE).then(c => c.put('/index.html', copy)); return r;
+    }).catch(() => caches.match('/index.html')));
+    return;
+  }
+  e.respondWith(caches.match(e.request).then(hit => hit || fetch(e.request).then(r => {
+    if (new URL(e.request.url).origin === location.origin) {
+      const copy = r.clone(); caches.open(CACHE).then(c => c.put(e.request, copy));
+    }
+    return r;
+  })));
+});
